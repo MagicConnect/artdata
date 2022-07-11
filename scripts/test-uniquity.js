47ae2055dcea98462f0269c4c5e3568bd0c4acdb
Kyle Kemp <kyle@seiyria.com>
2022-07-11 10:09:36 -0500

    move some sheets, add some sheets, add file name uniquity test

---
+++ scripts/test-uniquity.js
@@ -0,0 +1,21 @@
+
+const path = require('path');
+const readdir = require('recursive-readdir');
+
+const init = async () => {
+  const files = await readdir('assets/art');
+  
+  const fileHash = {};
+
+  files.forEach(file => {
+    const fileName = path.basename(file, '.png');
+    if(fileHash[fileName]) {
+      console.error(`Duplicate file name: ${fileName}`);
+      process.exit(1);
+    }
+
+    fileHash[fileName] = true;
+  })
+};
+
+init();
